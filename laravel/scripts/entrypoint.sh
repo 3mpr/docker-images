0356fcfadb276a87badd58b8931bedbec06eb1ff
3mpr <f.indot@students.ephec.be>
2017-02-02 23:00:25 +0100

    Imroved script readability & included webpack start within laravel image.

--- laravel/scripts/entrypoint.sh
+++ laravel/scripts/entrypoint.sh
@@ -1,30 +1,46 @@
 #!/usr/bin/env sh
 
-cd /home/www-data
+HOME_DIR='/home/www-data'
+APP_DIR="$HOME_DIR/app"
+LOG_DIR="$APP_DIR/storage/logs"
 
 echo -en "\nStarting laravel container...\n"
 
 if [ ! "$(ls -A /home/www-data/app)" ]; then
 
-    echo -en "\nLaravel installation not detected, installing...\n"
+    echo -en "\nLaravel installation not detected, installing...\n\n"
 
+    cd $HOME_DIR
     composer create-project --prefer-dist laravel/laravel tmp
 
     for x in tmp/* tmp/.[!.]* tmp/..?*; do
-        if [ -e "$x" ]; then mv -- "$x" app/; fi
+        if [ -e "$x" ]; then mv -- "$x" $APP_DIR; fi
     done
 
     rmdir tmp
 
-    cd app
+    cd $APP_DIR
     npm install
+    sed -i 's/webpack-dev-server.js/webpack-dev-server.js --public/'\
+     $APP_DIR/package.json
 
+    chown -R www-data $APP_DIR
     echo -en "\nDone.\n"
 
 fi
 
-cd /home/www-data/app
+case $MIX_ENV in
+    dev|watch|hot|production )
+        ;;
+    * )
+        MIX_ENV='dev'
+        ;;
+esac
+
+cd $APP_DIR
 su www-data
-php artisan serve --host 0.0.0.0
+
+php artisan serve --host 0.0.0.0 > $LOG_DIR/runtime.log 2>&1 &
+npm run $MIX_ENV
 
 exit $?
